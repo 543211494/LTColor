44919ca8c9159572b0821c07c0380e0e87e7e80f
lzy <lha602@163.com>
2022-11-23 22:50:40 +0800

    -

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <QApplication>
 #include "MainWidget.h"
 
-#pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     MainWidget mainWidget;
